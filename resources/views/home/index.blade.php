815f3099f54b77d4726364c43ff37e78106f7fb4
Shawnn1 <seyit.kilic1@outlook.com>
2022-03-18 01:01:05 +0300

    first commit

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Welcome to sa</title>
+</head>
+<body>
+
+<h1>first laravel</h1>
+<p>This is a paragraph.</p>
+
+<a href="{{route('anasayfa')}}">Ana sayfa</a>
+</body>
+</html>
